2e0a9552ab13184c38608811a9e07d2744a7fbf8
Kamil Wiater <kamilwiater.uj@gmail.com>
2023-04-05 11:00:04 +0200

    JS_IMPLEMENTED

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// FORM VALIDATION
+
 const dayInpRef = document.querySelector(".fi1");
 const monthInpRef = document.querySelector(".fi2");
 const yearInpRef = document.querySelector(".fi3");
@@ -6,15 +8,18 @@ const inpOne = document.querySelector(".inp1");
 const inpTwo = document.querySelector(".inp2");
 const inpThree = document.querySelector(".inp3");
 
-console.log(inpOne);
+const date = new Date();
+const currentYear = date.getFullYear();
+const currentMonth = date.getMonth() + 1;
+const currentDay = date.getDate();
 
 dayInpRef.addEventListener("change", () => {
   if (!dayInpRef.value < 0 || dayInpRef.value > 31) {
-    inpOne.children[4].innerText = "Must be a valid day";
+    inpOne.children[3].innerText = "Must be a valid day";
     inpOne.children[0].style.color = "red";
     inpOne.children[2].style.borderColor = "red";
   } else {
-    inpOne.children[4].innerText = "";
+    inpOne.children[3].innerText = "";
     inpOne.children[0].style.color = "#716f6f";
     inpOne.children[2].style.borderColor = "#dcdcdc";
   }
@@ -33,9 +38,6 @@ monthInpRef.addEventListener("change", () => {
 });
 
 yearInpRef.addEventListener("change", () => {
-  const date = new Date();
-  const currentYear = date.getFullYear();
-
   if (yearInpRef.value > currentYear) {
     inpThree.children[3].innerText = "Must be in the past";
     inpThree.children[0].style.color = "red";
@@ -45,4 +47,44 @@ yearInpRef.addEventListener("change", () => {
     inpThree.children[0].style.color = "#716f6f";
     inpThree.children[2].style.borderColor = "#dcdcdc";
   }
+});
+
+//CALCULATION
+
+const formRef = document.querySelector("form");
+
+const yearsSpan = document.querySelector(".years");
+const monthsSpan = document.querySelector(".months");
+const daysSpan = document.querySelector(".days");
+
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let birthdate = [];
+
+  birthdate.push(yearInpRef.value, monthInpRef.value, dayInpRef.value);
+
+  const calculateAge = (birthdate) => {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (months < 0 || (months === 0 && days < 0)) {
+      years--;
+      months += 12;
+    }
+
+    if (days < 0) {
+      months--;
+      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    yearsSpan.innerText = years;
+    monthsSpan.innerText = months;
+    daysSpan.innerText = days;
+  };
+
+  calculateAge(birthdate);
 });
